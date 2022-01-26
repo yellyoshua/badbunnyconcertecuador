49e309ec28564e4131aa8afcdf5de004fc1c817e
yellyoshua <usuario1529yoshua@hotmail.com>
2022-01-26 16:26:15 -0500

    PriceTiers

--- src/components/PriceTiers.tsx
+++ src/components/PriceTiers.tsx
@@ -1,4 +1,3 @@
-import Box from './Box';
 import Title from './Title';
 import { parsePrice } from '../utils';
 import store from '../store';
@@ -9,14 +8,37 @@ function PriceTiers() {
   return (
     <div>
       <Title content="Niveles de precios" />
-      <div>
-        {priceTiers.map((price, key) => (
-          <Box
-            key={`${price.name}-${key}`}
-            value={price.name}
-            label={parsePrice(price.price)}
-          />
-        ))}
+      <div className="py-3">
+        <table className="table-auto md:table-fixed border border-white text-white m-auto">
+          <thead>
+            <tr>
+              <th className="border border-white p-1 font-sans">Ubicación</th>
+              <th className="border border-white p-1 font-sans">Tickets</th>
+              <th className="border border-white p-1 font-sans">Vendidos</th>
+              <th className="border border-white p-1 font-sans">Agotado</th>
+              <th className="border border-white p-1 font-sans">Precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {priceTiers.map((tier, key) => (
+              <tr key={`${tier.name}-${key}`}>
+                <td className="border border-white p-1 text-sm">{tier.name}</td>
+                <td className="border border-white p-1 text-center font-sans text-sm">
+                  {tier.quantity}
+                </td>
+                <td className="border border-white p-1 text-center font-sans text-sm">
+                  {tier.quantity_sold}
+                </td>
+                <td className="border border-white p-1 text-center font-sans text-sm">
+                  {tier.sold_out ? 'x' : '-'}
+                </td>
+                <td className="border border-white p-1 text-center font-sans text-sm">
+                  {parsePrice(tier.price)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
